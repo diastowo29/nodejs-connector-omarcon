98b29e0715404b874052810cd211b35007008724
Diastowo <diastowo@gmail.com>
2023-10-17 17:17:14 +0700

    fixing product

--- payload/cifhelper.js
+++ payload/cifhelper.js
@@ -58,38 +58,19 @@ const cifPayload = async function (msg, brand_id, user_ticket_id, product_field_
     var msgObj = {};
     let username = msg.full_name
     let userid = msg.user_id
-    let ticket_external_id = `tokped-ticket-${userid}-${msg.msg_id}-${Date.now()}`;
     let ticket_thread_id = `tokped-thread-${userid}-${msg.shop_id}-${brand_id}`;
     let author_external_id = Buffer.from(`tokped::${userid}`).toString('base64');
     let product_id = msg.payload.product.product_id;
     let attachment_type = msg.payload.attachment_type;
-    // let msg_type = msg.type;
+    let ticket_external_id = `tokped-ticket-${userid}-${msg.msg_id}-${Date.now()}`;
+    let msg_encode;
     let msg_content = msg.message;
-    msgObj = {
-        external_id: ticket_external_id,
-        thread_id: ticket_thread_id,
-        created_at: new Date().toISOString(),
-        message: msg_content,
-        author: {
-            external_id: author_external_id,
-            name: username
-        },
-        fields:[{
-            id: 'subject',
-            value: 'Incoming Tokopedia Chat from: ' + username
-        },{
-            id: user_ticket_id.toString(),
-            value: userid
-        }, {
-            id: product_field_id.toString(),
-            value: product_id
-        }],
-        allow_channelback: true
-    }
     if (attachment_type == 3) {
-      let html_msg_content = '';
+      let product_encode = msg.payload.product.product_url.toString('base64')
+      ticket_external_id = `tokped-ticket-${userid}-${msg.msg_id}-${product_encode}`;
+      // let html_msg_content = '';
       // html_msg_content = html_msg.productHtml(msg.payload.product.name, msg.payload.product.price, msg.payload.product.product_url)
-      msg_content = 'Product: ' + product_id
+      msg_content = `Product: ${product_id}\nProduct URL : ${msg.payload.product.product_url}`
       // msgObj['html_message'] = html_msg_content
       msgObj['display_info'] = [{
         type: 'product',
@@ -102,9 +83,32 @@ const cifPayload = async function (msg, brand_id, user_ticket_id, product_field_
     } else if (attachment_type == 2) {
       let image_url = msg.payload.image.image_url;
       // let ext = mime.extension(mime.lookup(image_url))
-      // console.log(ext)
       msgObj['file_urls'] = [`/api/v1/file/image.jpeg?source=${image_url}`]
+    } else if (attachment_type == 0) {
+      msg_encode = msg.message.toString('base64')
+      ticket_external_id = `tokped-ticket-${userid}-${msg.msg_id}-${msg_encode}`;
     }
+    msgObj = {
+      external_id: ticket_external_id,
+      thread_id: ticket_thread_id,
+      created_at: new Date().toISOString(),
+      message: msg_content,
+      author: {
+          external_id: author_external_id,
+          name: username
+      },
+      fields:[{
+          id: 'subject',
+          value: 'Incoming Tokopedia Chat from: ' + username
+      },{
+          id: user_ticket_id.toString(),
+          value: userid
+      }, {
+          id: product_field_id.toString(),
+          value: product_id
+      }],
+      allow_channelback: true
+  }
   return msgObj;
 }
 
--- routes/api.js
+++ routes/api.js
@@ -104,8 +104,6 @@ router.post('/chat', async function(req, res, next) {
     let cifPayload = await (cif.cifPayload(req.body, 0, 0, '22587396407065'));
 	let external_resource_array = [cifPayload];
     let shop_id = req.body.shop_id;
-
-    console.log(JSON.stringify(req.body))
     connection.findOne({
         where: {
             shop_id: shop_id,
